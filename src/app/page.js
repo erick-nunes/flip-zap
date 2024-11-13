b3f2f157814aabd0da0e7d5479a0b7e2571d8b70
Erick Nunes <eu@ericknunes.dev>
2024-11-12 23:37:58 -0300

    Adicionando Direitos

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen p-8 bg-gray-100">
       <WhatsAppLinkGenerator />
+      <div className='text-center text-slate-800 mt-6 text-sm font-thin antialiased'> Desenvolvido por <a href='https://ericknunes.dev' target='_blank' className='text-blue-500'>Erick Nunes</a></div>
     </main>
   )
 }
